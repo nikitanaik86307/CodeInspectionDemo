d09b447af24253d9624108b836385ea52be2785a
nikitanaik86307 <2021wa86307@wilp.bits-pilani.ac.in>
2025-06-15 21:21:41 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,12 @@
-package com.example;
+package com.nikitanaik8327;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class CodeInspectionApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(CodeInspectionApplication.class, args);
     }
 }
